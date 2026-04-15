9fb6635aa89d86393cfea114d6c71021587ce50f
Zavier <ZavierPei@proton.me>
2026-04-15 21:30:31 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,7 @@
+# 主要用来学习和测试git相关内容
+
+# 测试一下
+
+ssh提交密钥和签名密钥测设
+
+windows github desktop gpg签名测试
---
+++ test1.py
@@ -0,0 +1,8 @@
+print("hello wrld")
+
+print("1password托管gitssh密钥测试")
+2026-01-19 19:36
+
+gpg 子密钥签名测试
+gpg 独立子密钥签名测试
+windows github desktop push测试
